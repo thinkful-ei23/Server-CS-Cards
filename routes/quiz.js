1d714a3cd1d4d48e653ecc1594c97775ef54d77a
john <johnjacobcosgrove@gmail.com>
2018-10-15 19:17:55 -0400

    added submit answer endpoint

--- routes/quiz.js
+++ routes/quiz.js
@@ -11,4 +11,13 @@ router.get('/quiz',(req,res,next)=>{
     res.json(questionList.head.value.question)
 })
 
+router.post('/submit',(req,res,next)=>{
+    let {answer} = req.body
+    answer = answer.toLowerCase().trim(' ')
+    if(questionList.head.value.answer === answer){
+        res.json({answer:'correct'})
+    }else{
+        res.json({answer:'incorrect'})
+    }
+})
 module.exports = router
