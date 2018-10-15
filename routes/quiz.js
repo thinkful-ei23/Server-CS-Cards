907e6b6a49d46e512b60b62faa28081224f3c4cd
john <johnjacobcosgrove@gmail.com>
2018-10-15 18:53:02 -0400

    added sumbit endpoint

--- routes/quiz.js
+++ routes/quiz.js
@@ -7,6 +7,18 @@ const questionList = require('../utils/create-linked-list')
 const router = express.Router();
 
 
-router.get('/api/quiz',(req,res,next)=>{
+router.get('/quiz',(req,res,next)=>{
     res.json(questionList.first.question)
 })
+
+
+router.get('/submit',(req,res,next)=>{
+    const {answer} = req.body
+    if(questionList.first.answer === answer){
+        res.json({answer:'correct'})
+    }else{
+        res.json({answer:'incorrect'})
+    }
+})
+
+module.exports = router
