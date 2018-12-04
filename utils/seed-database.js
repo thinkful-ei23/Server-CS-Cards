d4b3ae565545e91cb7187d62b41d359447d1fdd2
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-04 11:25:39 -0500

    added db/seed/quizStats.json so demo would work without errors

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,15 +5,19 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 
 const User = require('../models/user');
+const QuizStats = require('../models/quizStat');
 
 const seedUsers = require('../db/seed/users');
+const seedQuizStats = require('../db/seed/quizStats');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
     return Promise.all([
       User.insertMany(seedUsers),
-      User.createIndexes()
+      QuizStats.insertMany(seedQuizStats),
+      User.createIndexes(),
+      QuizStats.createIndexes()
     ]);
   })
   .then((results) => {
